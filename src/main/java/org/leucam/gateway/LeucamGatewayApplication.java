c850e77ce516681bfed1c2fa178421a6ba3dad90
Giuseppe VINCENZI <gdvincenzi@airfrance.fr>
2020-04-05 15:11:17 +0200

    First commit

---
+++ src/main/java/org/leucam/gateway/LeucamGatewayApplication.java
@@ -0,0 +1,13 @@
+package org.leucam.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeucamGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeucamGatewayApplication.class, args);
+    }
+
+}
